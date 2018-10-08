601c3a27f91d78e60c9e17795193d74656903a0b
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2018-10-08 11:48:35 +0200

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Enemigos/Enemigo.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour {
+
+    [Header("ESTADO")]
+    [SerializeField] bool estaVivo = true;
+    [SerializeField] int vida = 10;
+
+    [Header("ATAQUE")]
+    [SerializeField] int distanciaDeteccion = 5;
+    [SerializeField] int danyo = 2;//Daño que infringe
+
+    [Header("REFERENCIAS")]
+    [SerializeField] GameObject personaje;
+
+    //int distanciaExplosion;
+    private void Start() {
+        
+    }
+
+    public int DetectarDistanciaAlPersonaje() {
+
+        return 0;
+    }
+    public void Morir() {
+        Debug.Log("MURIENDO");
+        /*
+         1. Indicar que está muerto.
+         2. Sistema de particulas
+         3. Gritos horribles de dolor / Despedirse de los seres querido
+         4. Destruir el enemigo
+         5. ¿Aumentar salud? ¿Increntar puntuacion? ...
+         */
+        estaVivo = false;
+    }
+
+    public void Atacar() {
+
+    }
+
+    public void RecibirDanyo(int danyo) {
+        Debug.Log("RECIBIENDO DAÑO");
+        vida = vida - danyo;
+        if (vida <= 0) {
+            vida = 0;
+            Morir();
+        }
+    }
+
+    /*
+    private void OnMouseDown() {
+        Debug.Log("PULSADO CON EL RATON");
+        RecibirDanyo(10);
+    }
+    */
+
+}
---
+++ Assets/_GameAssets/Scripts/Personaje/Player.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private const int NUM_ARMAS = 4;
+    [SerializeField] int armaActiva = 0;
+    [SerializeField] private bool estaVivo;
+    [SerializeField] private int vidaActual;
+    [SerializeField] private int vidaMaxima;
+    [SerializeField] GameObject[] armas = new GameObject[NUM_ARMAS];
+
+    private void Start() {
+        /*SOLUCION CUTRE
+         armas[0].SetActive(true);
+         armas[1].SetActive(false);
+         armas[2].SetActive(false);
+         armas[3].SetActive(false);
+        */
+        //BUCLE FOR
+        /*
+        for(int i = 0; i < NUM_ARMAS; i++) {
+            armas[i].SetActive(false);
+        }
+        */
+        //BUCLE FOR EACH
+        ActivarArma(armaActiva);
+    }
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            ActivarArma(0);
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            ActivarArma(1);
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            ActivarArma(2);
+        } else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            ActivarArma(3);
+        }
+
+    }
+
+    private void ActivarArma(int armaActiva) {
+        DesactivarArmas();
+        armas[armaActiva].SetActive(true);
+    }
+
+    private void DesactivarArmas() {
+        foreach (var arma in armas) {
+            arma.SetActive(false);
+        }
+    }
+
+
+    public void IncrementarVida(int incrementoVida) {
+        vidaActual = vidaActual + incrementoVida;
+        if (vidaActual > vidaMaxima) {
+            vidaActual = vidaMaxima;
+        }
+    }
+
+    public void CambiarArma() {
+
+    }
+
+    public void Atacar() {
+
+    }
+    
+    public void RecibirDanyo(int danyo) {
+        vidaActual = vidaActual - danyo;
+        if (vidaActual <= 0) {
+            vidaActual = 0;
+            Morir();
+        }
+    }
+
+    public void Morir() {
+
+    }
+
+}
